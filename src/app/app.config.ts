70af5468d40fa6b86c2f004c08e77b1705807b6c
Jonathan <jogaj_26@hotmail.com>
2026-01-02 04:15:36 +0000

    Route View animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,13 @@ import {
 	withInterceptorsFromDi,
 } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+	createUrlTreeFromSnapshot,
+	provideRouter,
+	ViewTransitionInfo,
+	withComponentInputBinding,
+	withViewTransitions,
+} from '@angular/router';
 import { ErrorInterceptor } from '../interceptors/error.interceptor';
 import { TokenInterceptor } from '../interceptors/token.interceptor';
 import { routes } from '../routing/app.routes';
@@ -13,10 +19,32 @@ import { routes } from '../routing/app.routes';
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes, withComponentInputBinding()),
+		provideRouter(
+			routes,
+			withComponentInputBinding(),
+			withViewTransitions({ onViewTransitionCreated }),
+		),
 		provideHttpClient(withFetch(), withInterceptorsFromDi()),
 		// provideHttpClient(withInterceptors([ErrorInterceptor]))
 		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 		{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
 	],
 };
+
+async function onViewTransitionCreated(info: ViewTransitionInfo) {
+	const fromRoute = createUrlTreeFromSnapshot(info.from, []).toString();
+	const toRoute = createUrlTreeFromSnapshot(info.to, []).toString();
+	const doc = document.documentElement;
+
+	if (
+		((fromRoute === '/new' || fromRoute.includes('/edit/')) &&
+			toRoute === '/main-view') ||
+		(fromRoute != '' && toRoute === '/login')
+	) {
+		doc.classList.add('slide-backward');
+		doc.classList.remove('slide-forward');
+	} else {
+		doc.classList.add('slide-forward');
+		doc.classList.remove('slide-backward');
+	}
+}
